4c3656f6869dfa02ffcf645576a371315a51cf54
Ryan <ryanbprog@gmail.com>
2024-06-28 21:01:38 +0100

    inital structure started

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
 function App() {
-  return <>test</>;
+  return (
+    <>
+      <header>header</header>
+      <main>
+        <form>
+          <div></div>
+          <div>
+            <div>
+              <h1>Personal Info</h1>
+              <p>Please provide your personal information.</p>
+              <label htmlFor="name">
+                <input type="text" id="name" />
+              </label>
+            </div>
+          </div>
+        </form>
+      </main>
+      <footer>footer</footer>
+    </>
+  );
 }
 
 export default App;
