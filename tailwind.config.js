508e058acc79164990d47e39b855c1e6e9e918b3
Ryan <ryanbprog@gmail.com>
2024-06-29 07:35:47 +0100

    background images added to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        sidebarMobile: "url(./assets/images/bg-sidebar-mobile.svg)",
+        sidebarDesktop: "url(./assets/images/bg-sidebar-desktop.svg)",
+      },
+    },
   },
   plugins: [],
 };
