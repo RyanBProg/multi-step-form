05dee77b79bad465a6df1a8f0fcf5586288168e9
Ryan <ryanbprog@gmail.com>
2024-07-01 13:58:07 +0100

    pricing data added

---
+++ src/data/pricingData.ts
@@ -0,0 +1,26 @@
+export const pricingData = {
+  monthly: {
+    plans: {
+      arcade: 9,
+      advanced: 12,
+      pro: 15,
+    },
+    addOns: {
+      onlineService: 1,
+      largerStorage: 2,
+      customProfile: 2,
+    },
+  },
+  annual: {
+    plans: {
+      arcade: 90,
+      advanced: 120,
+      pro: 150,
+    },
+    addOns: {
+      onlineService: 10,
+      largerStorage: 20,
+      customProfile: 20,
+    },
+  },
+};
