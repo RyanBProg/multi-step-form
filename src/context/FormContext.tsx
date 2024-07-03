9f0309a7ea79f9448fbdbeb2eff8fca9051b7812
Ryan <ryanbprog@gmail.com>
2024-07-03 10:39:36 +0100

    form state moved to context: -usecontext set up with custom exported hook -reducer logic moved to context file

--- src/context/FormContext.tsx
+++ src/context/FormContext.tsx
@@ -1,26 +1,115 @@
-import { createContext, ReactNode, useReducer, Dispatch } from "react";
 import {
-  formReducer,
-  formTemplate,
-  UserFormData,
-  Action,
-} from "../state/formReducer";
+  useContext,
+  createContext,
+  ReactNode,
+  useReducer,
+  Dispatch,
+} from "react";
 
-interface FormContextType extends UserFormData {
+type StateType = {
+  name: string;
+  email: string;
+  number: string;
+  subscriptionType: string;
+  addOns: string[];
+  currentStep: number;
+};
+
+const initialState: StateType = {
+  name: "",
+  email: "",
+  number: "",
+  subscriptionType: "",
+  addOns: [],
+  currentStep: 1,
+};
+
+type ActionType =
+  | "name_changed"
+  | "email_changed"
+  | "number_changed"
+  | "subscription_changed"
+  | "addOn_removed"
+  | "addOn_added"
+  | "increased_step"
+  | "decreased_step";
+
+type Action = {
+  type: ActionType;
+  payload?: string;
+};
+
+export function formReducer(state: StateType, action: Action) {
+  switch (action.type) {
+    case "name_changed":
+      return { ...state, name: action.payload || state.name };
+
+    case "email_changed":
+      return { ...state, email: action.payload || state.email };
+
+    case "number_changed":
+      return { ...state, number: action.payload || state.number };
+
+    case "subscription_changed":
+      return {
+        ...state,
+        subscriptionType: action.payload || state.subscriptionType,
+      };
+
+    case "addOn_removed":
+      if (!action.payload) return state;
+      const newAddOns = state.addOns.filter(
+        (addOn) => addOn !== action.payload
+      );
+      return { ...state, addOns: [...newAddOns] };
+
+    case "addOn_added":
+      if (!action.payload) return state;
+      if (state.addOns.includes(action.payload)) {
+        return state;
+      } else {
+        return { ...state, addOns: [...state.addOns, action.payload] };
+      }
+
+    case "increased_step":
+      return { ...state, currentStep: state.currentStep + 1 };
+
+    case "decreased_step":
+      return { ...state, currentStep: state.currentStep - 1 };
+
+    default:
+      return state;
+  }
+}
+
+interface FormContextType extends StateType {
   dispatch: Dispatch<Action>;
 }
+
+export const FormContext = createContext<FormContextType | undefined>(
+  undefined
+);
 
 type Props = {
   children: ReactNode;
 };
 
 export default function FormProvider({ children }: Props) {
-  const [state, dispatch] = useReducer(formReducer, formTemplate);
-  const FormContext = createContext<FormContextType | undefined>(undefined);
+  const [state, dispatch] = useReducer(formReducer, initialState);
 
   return (
     <FormContext.Provider value={{ ...state, dispatch }}>
       {children}
     </FormContext.Provider>
   );
+}
+
+export function useFormContext() {
+  const context = useContext(FormContext);
+
+  if (!context) {
+    throw new Error("FormContext must be used within a FormProvider");
+  }
+
+  return context;
 }
